d2318ffeaf9be27220d914d2f627e155cd23489f
VivianShibata <110480785+VivianShibata@users.noreply.github.com>
2023-04-21 21:19:12 -0300

    Add files via upload

---
+++ Ponderada_1-1.ino
@@ -0,0 +1,9 @@
+
+//Vivian
+//
+#include <Arduino.h> // to include an arduino library
+
+void setup() { // void setup is "static" while void loop works like the "for" function
+    Serial.begin(9600); // establish serial communication
+}
+

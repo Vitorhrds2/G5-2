94a41c63ab7c01cf92d6188db328371df4492cca
LucasG99 <lucas.galvao@sou.inteli.edu.br>
2023-04-21 21:26:33 -0300

    minha parte (LG)

--- Ponderada_1-1/Ponderada_1-1.ino
+++ Ponderada_1-1/Ponderada_1-1.ino
@@ -7,3 +7,11 @@ void setup() { // void setup is "static" while void loop works like the "for" fu
     Serial.begin(9600); // establish serial communication
 }
 
+//Lucas Galvão
+void loop() {
+    // Entrada de dados do usuário
+    String nome;
+    Serial.print("Digite o seu nome: ");
+    while (!Serial.available());
+    nome = Serial.readStringUntil('\n');
+    nome.trim(); 
